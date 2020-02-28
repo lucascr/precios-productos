82cdc8d113a2a7a459cce57fd6bb396256ff02d0
lucascr <lucascr@gmail.com>
2020-02-28 04:14:42 -0600

    Fri, Feb 28, 2020  4:14:42 AM Cambios

--- productos-precios-app/src/containers/buscador.jsx
+++ productos-precios-app/src/containers/buscador.jsx
@@ -69,7 +69,7 @@ class Buscador extends Component {
                     type = {'primary'} 
                     title = {'Submit'} 
                     style={buttonStyle}
-                    /> { /*Submit */ }
+                    /> 
             </form>
         )
     }
