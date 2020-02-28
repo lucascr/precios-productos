77c81511c813ebf830f2b8e8e3642e00f7bd047d
lucascr <lucascr@gmail.com>
2020-02-28 04:26:02 -0600

    Fri, Feb 28, 2020  4:26:01 AM Cambios

--- productos-precios-app/src/containers/buscador.jsx
+++ productos-precios-app/src/containers/buscador.jsx
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';  
 
-/* Import Components */
-/*import CheckBox from '../components/CheckBox';  
+import CheckBox from '../components/CheckBox';  
 import Input from '../components/Input';  
 import TextArea from '../components/TextArea';  
-import Select from '../components/Select';*/
+import Select from '../components/Select';
 import Button from '../components/Button'
 
 class Buscador extends Component {  
