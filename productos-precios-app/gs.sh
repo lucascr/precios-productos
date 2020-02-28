d919816587b1955f7eaf364de86e39b84dbc7a36
Cloud User <centos@ip-172-26-15-81.ec2.internal>
2020-02-28 09:20:23 +0000

    Fri Feb 28 09:20:23 UTC 2020 Cambios

---
+++ productos-precios-app/gs.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "$1 `date` Cambios" 
+git push -u origin master
---
+++ productos-precios-app/src/containers/buscador.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';  
+
+/* Import Components */
+import CheckBox from '../components/CheckBox';  
+import Input from '../components/Input';  
+import TextArea from '../components/TextArea';  
+import Select from '../components/Select';
+import Button from '../components/Button'
+
+class Buscador extends Component {  
+    constructor(props) {
+    }
+    render() {
+        return (
+            <form className="container-fluid" onSubmit={this.handleFormSubmit}>
+                <label>
+                Barcode:
+                <Input inputType={'text'}
+                    title= {'barcode'} 
+                    name= {'barcode'}
+                    value={''} 
+                    placeholder = {'scan barcode'}                    
+                    /> {/* Barcode search */}
+
+                </label>
+                <Button 
+                    action = {this.handleFormSubmit}
+                    type = {'primary'} 
+                    title = {'Submit'} 
+                    style={buttonStyle}
+                    /> { /*Submit */ }
+            </form>
+        )
+    }
+}  
+export default Buscador;
