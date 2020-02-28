5e674b28a0d54f71b8cb893ff5182af49816947c
lucascr <lucascr@gmail.com>
2020-02-28 04:31:14 -0600

    Fri, Feb 28, 2020  4:31:13 AM Cambios

--- productos-precios-app/src/containers/buscador.jsx
+++ productos-precios-app/src/containers/buscador.jsx
@@ -38,6 +38,7 @@ class Buscador extends Component {
                         type="text"
                         onChange={this.myChangeHandler}
                         placeholder="Scan Barcode"
+                        autocomple="off"
                     />
                 </div>
 
